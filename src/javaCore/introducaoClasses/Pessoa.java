33816fbaae016a301b2cc0f154a84c146cab6415
Raphael <raphael.siqueira4@gmail.com>
2024-08-28 23:57:52 -0300

    Introdução a Orientação à Objetos

--- src/javaCore/introducaoClasses/Pessoa.java
+++
@@ -1,6 +0,0 @@
-package academy.devDojo.src.javaCore.introducaoClasses;
-
-public class Pessoa {
-    public String nome;
-    public int idade;
-}
---
+++ src/javaCore/introducaoClasses/teste/EstudanteTeste01.java
@@ -0,0 +1,18 @@
+package academy.devDojo.src.javaCore.introducaoClasses.teste;
+
+import academy.devDojo.src.javaCore.introducaoClasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+
+        //Sempre para criar objetos é necessário utilizar "new" seguido do nome do objeto que eu quero utilizar.
+        Estudante estudante = new Estudante();
+        estudante.nome = "Alana";
+        estudante.idade = 22;
+        estudante.sexo = 'F';
+
+        System.out.println("Nome: " + estudante.nome);
+        System.out.println("Idade: " + estudante.idade);
+        System.out.println("Sexo: " + estudante.sexo);
+    }
+}
