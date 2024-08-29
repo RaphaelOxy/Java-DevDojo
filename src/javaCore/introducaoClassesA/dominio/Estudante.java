b67f8786b4ba60495e64844e88aa35d2aeecc42c
Raphael <raphael.siqueira4@gmail.com>
2024-08-29 12:39:31 -0300

    Introdução Classes OO

--- src/javaCore/introducaoClassesA/dominio/Estudante.java
+++ src/javaCore/introducaoClassesA/dominio/Estudante.java
@@ -1,6 +1,10 @@
 package academy.devDojo.src.javaCore.introducaoClassesA.dominio;
 
+
+import java.util.Scanner;
+
 public class Estudante {
+    //Scanner sc = new Scanner(System.in);
     public String nome;
     public int idade;
     public char sexo;
--- src/javaCore/introducaoClassesA/teste/EstudanteTeste01.java
+++ src/javaCore/introducaoClassesA/teste/EstudanteTeste01.java
@@ -8,8 +8,12 @@ public class EstudanteTeste01 {
         //Sempre para criar objetos é necessário utilizar "new" seguido do nome do objeto que eu quero utilizar.
         Estudante estudante = new Estudante();
         estudante.nome = "Alana";
-        estudante.idade = 22;
+        //estudante.idade = 22;
         estudante.sexo = 'F';
+
+        System.out.println("Nome: " + estudante.nome);
+        System.out.println("Idade: " + estudante.idade);
+        System.out.println("Sexo: " + estudante.sexo);
 
         System.out.println("Nome: " + estudante.nome);
         System.out.println("Idade: " + estudante.idade);
